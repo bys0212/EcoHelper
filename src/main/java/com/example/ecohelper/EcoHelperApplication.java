572f3132fdb81b52b0e8d22146a30b2474be398c
KYJ <heroria0503@gmail.com>
2024-09-11 13:00:46 +0900

    협업 테스트

---
+++ src/main/java/com/example/ecohelper/EcoHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecohelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecohelper/EcoHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecohelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
